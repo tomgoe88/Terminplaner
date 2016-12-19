9f984d5392ff2189bccc98a2d6d049bd1776f32c
tomgoe88 <tom.goe88@googlemail.com>
2016-12-19 06:55:14 +0100

    Erster Teil... Es wird direkt ein neuer Kalender gestartet. Aus der Terminanzeige.php werden nur die Logiken für die Button nächster Monat etc übernommen

--- Kalenderkonfig.php
+++ Kalenderkonfig.php
@@ -1,21 +1,22 @@
-<?php
-$mitarbeiterNr= 0;
-function weitererMitarbeiter()
-{
-    global $mitarbeiterNr;
-    $mitarbeiterNr=$mitarbeiterNr +1;
-    echo "hier kommt das weitere Formular Inputfeld mit Mitarbeiternummer";
-}
-?>
+
 
 <html xmlns="http://www.w3.org/1999/html">
     <head>
 
     </head>
     <body>
-        < action="KalenderkonfigAnzeige.php" method="post">
+        <form action="KalenderkonfigAnzeige.php" method="post">
             Hier wird angegeben wie viele Mitarbeiter
             Welche Öffnungszeiten
+            <?php
+            $mitarbeiterNr= 0;
+            function weitererMitarbeiter()
+            {
+                global $mitarbeiterNr;
+                $mitarbeiterNr=$mitarbeiterNr +1;
+                echo "hier kommt das weitere Formular Inputfeld mit Mitarbeiternummer";
+            }
+            ?>
 
         </form>
     </body>
